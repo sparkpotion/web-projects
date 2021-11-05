d4b6399fe90a378c9c44729a2f9988ba459c95e4
sparkpotion <317368223@qq.com>
2021-11-05 23:35:32 +0800

    第三次作业-lv3

--- 第三次作业-2021211717-葛彬/Level3.js
+++ 第三次作业-2021211717-葛彬/Level3.js
@@ -4,7 +4,7 @@ let arrl3 = [[1, [2, 3], 4], [5, [6, 7], 8, 9]];
 var out=[];
 function lp(arr){
 	for(let i=0;i<arr.length;i++){
-		if(arr[i].length&&arr[i].length>0){
+		if(arr[i].length){
 			lp(arr[i]);
 		}else{
 			out.push(arr[i]);
